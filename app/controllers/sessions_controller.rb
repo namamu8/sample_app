c253bb9761f3e14ebc5d5eeeb9fd7d8dffa55e23
minh <namamu8@gmail.com>
2021-05-19 07:39:39 +0700

    Fix syntax chapter 9
    Finished chapter 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_back_or user
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
-      render 'new'
+      flash[:danger] = "Invalid email/password combination" # Not quite right!
+      render "new"
     end
   end
 
