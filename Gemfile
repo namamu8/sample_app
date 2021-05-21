d62ec74cdff48a0651a032656a9f29e08d7a3e9a
minh <namamu8@gmail.com>
2021-05-21 10:43:34 +0700

    chapter 11 - done

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'bcrypt', '3.1.13'
 gem 'faker', '2.1.2'
 gem 'will_paginate', '3.1.8'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'awesome_nested_set', '~> 3.4.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use bootstrap
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,9 +35,19 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  host = "localhost:3000" # Don"t use this literally; use your local dev host instead
-  
-  config.action_mailer.default_url_options = { host: host, protocol: "http" }
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { host: host}
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["user"],
+    :password             => ENV["pass"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
